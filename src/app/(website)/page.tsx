edefc9782f76af3841262c500c8fc8e6d4052a8e
Hammad Sherani <hammadk426623@gmail.com>
2025-07-04 14:54:59 -0700

    changes

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -4,11 +4,13 @@ import React from 'react'
 function HomePage() {
   return (
     <div>
-      <h1 className='text-5xl font-bricolage'>This is Heading Featured  </h1>
+      <h1 className='text-5xl font-bricolage'>
+        This is Heading <span className='text-secondary-600'>Featured</span>
+      </h1>
 
       <button>
-        <Link href="/dashboard"> 
-        Go to Dashboard
+        <Link href="/dashboard">
+          Go to Dashboard
         </Link>
       </button>
     </div>
