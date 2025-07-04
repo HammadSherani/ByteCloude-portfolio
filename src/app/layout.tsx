e77fb7bd09ccedff611952d320b983a8ce1f3729
Hammad Sherani <hammadk426623@gmail.com>
2025-07-04 13:35:04 -0700

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
   title: "Byte Cloude",
   description: "Byte Cloude is a modern software company offering reliable and customized digital solutions. We specialize in web and mobile application development, cloud-based systems, and business automation to help businesses grow through technology.",
    icons: {
-    icon: "./favicon.png",
+    icon: "/favicon.png" // Use an absolute path from the public folder
   },
 };
 
